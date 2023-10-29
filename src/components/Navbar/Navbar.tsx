cb44ab21b528ac36329d6730236f1436370d4e68
Bryan-an <bryanandagoya@gmail.com>
2023-10-29 10:20:03 -0500

    Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,50 @@
+import { IUserModel } from '@/types/user';
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import SearchLineIcon from '@/icons/SearchLineIcon';
+import AddLineIcon from '@/icons/AddLineIcon';
 
 interface IProps {
   searchTerm: string;
   setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+  user?: IUserModel;
 }
 
-const Navbar: React.FC<IProps> = () => {
-  return <div>Navbar</div>;
+const Navbar: React.FC<IProps> = ({ searchTerm, setSearchTerm, user }) => {
+  const navigate = useNavigate();
+
+  if (!user) return null;
+
+  return (
+    <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7">
+      <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
+        <SearchLineIcon size={21} className="ml-1" />
+        <input
+          type="text"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search"
+          value={searchTerm}
+          onFocus={() => navigate('/search')}
+          className="p-2 w-full bg-white outline-none"
+        />
+      </div>
+      <div className="flex gap-3">
+        <Link to={`user-profile/${user?._id}`} className="hidden md:block">
+          <img
+            src={user.image}
+            alt="user profile image"
+            className="w-14 h-12 rounded-lg"
+          />
+        </Link>
+        <Link
+          to="create-pin"
+          className="bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center"
+        >
+          <AddLineIcon />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
